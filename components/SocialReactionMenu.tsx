c961dd146a0be5cedefc33a987285b21ba53db13
CowboyDiver <jandetlefsen@gmail.com>
2022-09-22 11:07:23 +0200

    feat: :sparkles: added Framer Motion and animated social emoji menu

--- components/SocialReactionMenu.tsx
+++ components/SocialReactionMenu.tsx
@@ -3,6 +3,7 @@ import { Frown, Heart, Smile, ThumbsDown, ThumbsUp } from "react-feather";
 import { theme } from "../styles/theme";
 import { Card } from "./Card";
 import { ReactionState, ReactionType } from "./helpers/ReactionState";
+import { motion } from "framer-motion";
 
 interface SocialReactionMenuProps {
   onClick?: (type: ReactionType) => void;
@@ -10,42 +11,44 @@ interface SocialReactionMenuProps {
 
 export function SocialReactionMenu({ onClick }: SocialReactionMenuProps) {
   return (
-    <Card padding="sm" shadow="sm">
-      <div>
-        <div css={{ padding: "5px", display: "flex", gap: "5px" }}>
-          <SocialMediaEmoji
-            color={theme.colors.blue.blue9}
-            onClick={() => onClick?.(ReactionState.like.text)}
-          >
-            <ThumbsUp />
-          </SocialMediaEmoji>
-          <SocialMediaEmoji
-            color={theme.colors.blue.blue9}
-            onClick={() => onClick?.(ReactionState.dislike.text)}
-          >
-            <ThumbsDown />
-          </SocialMediaEmoji>
-          <SocialMediaEmoji
-            color={theme.colors.red.red9}
-            onClick={() => onClick?.(ReactionState.love.text)}
-          >
-            <Heart />
-          </SocialMediaEmoji>
-          <SocialMediaEmoji
-            color={theme.colors.amber.amber10}
-            onClick={() => onClick?.(ReactionState.happy.text)}
-          >
-            <Smile />
-          </SocialMediaEmoji>
-          <SocialMediaEmoji
-            color={theme.colors.amber.amber10}
-            onClick={() => onClick?.(ReactionState.sad.text)}
-          >
-            <Frown />
-          </SocialMediaEmoji>
+    <motion.div initial={{ y: 20 }} animate={{ y: 0 }}>
+      <Card padding="sm" shadow="sm">
+        <div>
+          <div css={{ padding: "5px", display: "flex", gap: "5px" }}>
+            <SocialMediaEmoji
+              color={theme.colors.blue.blue9}
+              onClick={() => onClick?.(ReactionState.like.text)}
+            >
+              <ThumbsUp />
+            </SocialMediaEmoji>
+            <SocialMediaEmoji
+              color={theme.colors.blue.blue9}
+              onClick={() => onClick?.(ReactionState.dislike.text)}
+            >
+              <ThumbsDown />
+            </SocialMediaEmoji>
+            <SocialMediaEmoji
+              color={theme.colors.red.red9}
+              onClick={() => onClick?.(ReactionState.love.text)}
+            >
+              <Heart />
+            </SocialMediaEmoji>
+            <SocialMediaEmoji
+              color={theme.colors.amber.amber10}
+              onClick={() => onClick?.(ReactionState.happy.text)}
+            >
+              <Smile />
+            </SocialMediaEmoji>
+            <SocialMediaEmoji
+              color={theme.colors.amber.amber10}
+              onClick={() => onClick?.(ReactionState.sad.text)}
+            >
+              <Frown />
+            </SocialMediaEmoji>
+          </div>
         </div>
-      </div>
-    </Card>
+      </Card>
+    </motion.div>
   );
 }
 
