4ab40c54dfc1749d5188b908b3e2e0d4220d9a7d
CowboyDiver <jandetlefsen@gmail.com>
2022-09-27 14:24:46 +0200

    feat: :lipstick: new social icon animation

--- components/SocialReactionMenu.tsx
+++ components/SocialReactionMenu.tsx
@@ -10,48 +10,80 @@ interface SocialReactionMenuProps {
 }
 
 export function SocialReactionMenu({ onClick }: SocialReactionMenuProps) {
+  const outerWrapper = {
+    hidden: { y: 20, opacity: 0 },
+    show: { y: 0, opacity: 1 },
+  };
+
+  const container = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.05,
+        delayChildren: 0.3,
+      },
+    },
+  };
+
+  const child = {
+    hidden: { y: 10, scale: 0 },
+    show: { y: 0, scale: 1, transition: { type: "spring", bounce: 0.7 } },
+  };
+
   return (
-    <motion.div initial={{ y: 20 }} animate={{ y: 0 }}>
+    <motion.div variants={outerWrapper} initial="hidden" animate="show">
       <Card padding="sm" shadow="sm">
-        <div>
-          <div css={{ padding: "5px", display: "flex", gap: "5px" }}>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          css={{ padding: "5px", display: "flex", gap: "5px" }}
+        >
+          <motion.div variants={child} style={{ originY: 1 }}>
             <SocialMediaEmoji
               color={theme.colors.blue.blue9}
               onClick={() => onClick?.(ReactionState.like.text)}
             >
               <ThumbsUp />
             </SocialMediaEmoji>
+          </motion.div>
+          <motion.div variants={child} style={{ originY: 1 }}>
             <SocialMediaEmoji
               color={theme.colors.blue.blue9}
               onClick={() => onClick?.(ReactionState.dislike.text)}
             >
               <ThumbsDown />
             </SocialMediaEmoji>
+          </motion.div>
+          <motion.div variants={child} style={{ originY: 1 }}>
             <SocialMediaEmoji
               color={theme.colors.red.red9}
               onClick={() => onClick?.(ReactionState.love.text)}
             >
               <Heart />
             </SocialMediaEmoji>
+          </motion.div>
+          <motion.div variants={child} style={{ originY: 1 }}>
             <SocialMediaEmoji
               color={theme.colors.amber.amber10}
               onClick={() => onClick?.(ReactionState.happy.text)}
             >
               <Smile />
             </SocialMediaEmoji>
+          </motion.div>
+          <motion.div variants={child} style={{ originY: 1 }}>
             <SocialMediaEmoji
               color={theme.colors.amber.amber10}
               onClick={() => onClick?.(ReactionState.sad.text)}
             >
               <Frown />
             </SocialMediaEmoji>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </Card>
     </motion.div>
   );
 }
-
 interface SocialMediaEmojiProps {
   children: React.ReactNode;
   color?: string;
